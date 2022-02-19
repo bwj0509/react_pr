9fe8445c9d247a8a7fc15f6ba1e1b9c95968a93c
bwj59 <bwj59@naver.com>
2022-02-19 23:06:05 +0900

    pr

--- src/App.js
+++ src/App.js
@@ -3,43 +3,59 @@ import './App.css';
 
 function App() {
   
-  let [posts, changePosts] = useState(['창원 맛집','서울 맛집','인천 맛집']);
+  let [posts, changePosts] = useState(['남자코트 추천','강남 우동맛집','파이썬독학']);
   let [count, changeCount] = useState(0);
-  
+  let [modal, modalChange] = useState(false);
+  let [listNumber, listNumberChange] = useState(0);
+  let [addTitle, addTitleChange] =useState('');
+
   function change(){
     changeCount(count+1)
   }
 
-  function change_dosi(){
-    let newArray = [...posts]
-    newArray[0] = '대전 맛집'
-    changePosts(newArray);
+  function addPost(){
+    let Array = [addTitle,...posts]
+    changePosts(Array);
   }
-
 
   return (
     <div className="App">
       <div className='black-nav'>
         <div>개발 blog</div>
       </div>
-      <div className='list'>
-        <h3>{posts[0]} <span className='ddabong' onClick={change}>👍</span>{count}</h3>
-        <p>2월 17일 발행</p>
-        <hr />
-      </div>
-      <div className='list'>
-        <h3>{posts[1]}</h3>
-        <p>2월 17일 발행</p>
-        <hr />
-      </div>
-      <div className='list'>
-        <h3>{posts[2]}</h3>
-        <p>2월 17일 발행</p>
-        <hr />
-      </div>
-      <button onClick={change_dosi}>버튼</button>
+      {
+        posts.map(function(posts,i){
+          return(
+            <div className='list' key={i}>
+              <h3 onClick={ ()=>{listNumberChange(i)} }>{posts} <span className='ddabong' onClick={change}>👍</span>{count}</h3>
+              <p>2월 17일 발행</p>
+              <hr />
+            </div>
+          )
+        })
+      }
+    <div className='publish'>
+      <input onChange={(e)=>{addTitleChange(e.target.value)}}></input>
+      <button onClick={addPost}>저장</button>
+    </div>
+
+
+    <button onClick={()=>{modalChange(!modal)}}>열고닫기</button>
+    {modal === true? <Modal posts={posts} listNumber={listNumber}/> : null}
     </div>
   );
 }
+
+function Modal(props){
+  return(
+    <div className='modal'>
+      <h2>{ props.posts[props.listNumber] }</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+    
+  )
+}
+
 
 export default App;
